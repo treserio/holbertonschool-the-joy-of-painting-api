1dd544f7d33eef8ce84fe66afcacbeeba3e74c45
Tres Serio <treserio@hotmail.com>
2022-08-30 00:58:20 -0500

    setup all imports, +one example query for colors

--- load.py
+++ load.py
@@ -1,29 +1,100 @@
-import json
 import pandas as pd
 import re
 from datetime import datetime
 from sqlalchemy import create_engine
+from sqlalchemy.sql import select
 # bob_ross database must be setup first before this will run
 engine = create_engine('mysql://root@localhost:3306/bob_ross')
 
-colorsUsed = pd.read_csv('./TJOP - Colors Used')
-subjectMatter = pd.read_csv('./TJOP - Subject Matter')
+colors_used = pd.read_csv('./TJOP - Colors Used')
+subject_matter = pd.read_csv('./TJOP - Subject Matter')
 
-print(subjectMatter)
-print(type(subjectMatter))
-print(subjectMatter.__dict__)
+# print(colors_used)
+# print(type(colors_used))
+# print(colors_used.__dict__)
+# print('\n'.join(f'{col}' for col in colors_used.columns))
 
+###
+#   get our list of episode strings to use for joins in most tables, and set to a pandas dataframe
+###
+episode_DF = subject_matter['EPISODE']
+
+###
+#   create the pic_info table by filtering and adding various column data with pandas dataframes
+###
 episodeDates = [re.findall(r'[\w\s]\(([\d\w,\s]+)\)[$ ]?', line)[0] for line in open('TJOP - Episode Dates')]
-print(episodeDates)
+date_objects = [datetime.strptime(date, '%B %d, %Y') for date in episodeDates]
+# print(date_objects)
+date_DF = pd.DataFrame()
+date_DF['air_date'] = date_objects
 
-dtObjs = [datetime.strptime(date, '%B %d, %Y') for date in episodeDates]
+main_drop_list = [
+'index',
+'Black_Gesso',
+'Bright_Red',
+'Burnt_Umber',
+'Cadmium_Yellow',
+'Dark_Sienna',
+'Indian_Red',
+'Indian_Yellow',
+'Liquid_Black',
+'Liquid_Clear',
+'Midnight_Black',
+'Phthalo_Blue',
+'Phthalo_Green',
+'Prussian_Blue',
+'Sap_Green',
+'Titanium_White',
+'Van_Dyke_Brown',
+'Yellow_Ochre',
+'Alizarin_Crimson',
+'colors',
+'color_hex',
+]
 
-df = pd.DataFrame()
-df['air_date'] = dtObjs
+# create a clean dataframe to use for our main import
+main_DF = colors_used.drop(main_drop_list, axis = 1)
+# rename the episode column to allow the use of the EPISODE column
+main_DF.columns = main_DF.columns.str.replace('episode', 'episode_num')
+# concatonate all data frames into a single import source
+main_import = pd.concat([episode_DF, main_DF, date_DF], axis=1)
+# print(main_import.columns)
+# create our table using to_sql
+main_import.to_sql('pic_info', con=engine, if_exists='replace')
 
-stuff = pd.concat([colorsUsed, df], axis=1)
-print(stuff)
+###
+#   Create the colors table from the dataframes
+###
+colors_drop_list = ['index', 'painting_index', 'img_src', 'painting_title', 'season', 'episode', 'num_colors', 'youtube_src', 'colors', 'color_hex']
+# concate the episode_DF with the dataframe of dropped columns from colors_used
+colors_import = pd.concat([episode_DF, colors_used.drop(colors_drop_list, axis = 1)], axis=1)
+# print(colors_import.columns)
+# create our table using to_sql
+colors_import.to_sql('colors', con=engine, if_exists='replace')
 
-stuff.to_sql('pic_info', con=engine)
+###
+#   create list of hex values for colors_hex table
+###
+hex_DF = colors_used[['colors', 'color_hex']]
+# print(hex_DF)
+# setup key value pairs based on string input from rows of hex_DF, all col values of hex_DF start as strings
+hex_colors = {}
+for k, v in hex_DF.iterrows():
+    color_list = v.colors[1:-1].translate({ord(c): None for c in "'\\rn"}).split(', ')
+    hex_list = v.color_hex[1:-1].translate({ord(c): None for c in "'\\rn"}).split(', ')
+    hex_colors.update({color_list[i]:hex_list[i] for i in range(len(color_list))})
+hex_import = pd.DataFrame({'color': hex_colors.keys(), 'hex': hex_colors.values()})
+hex_import.to_sql('colors_hex', con=engine, if_exists='replace')
 
-# push dataframe to mysql
+###
+#   working with sqlAlchemy cursor example, get the list of colors from the colors table
+###
+# conn = engine.connect()
+# sql_exec = 'SELECT * FROM colors WHERE EPISODE = "S01E01"'
+# ret_val = conn.execute(sql_exec).mappings().all()[0]
+# print(ret_val)
+# return_colors = [ k for k, v in ret_val.items() if v == 1 ]
+# print('\return_colors\n', return_colors)
+# # close our sql connection
+# conn.close()
+
